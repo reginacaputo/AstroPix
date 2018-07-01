82020d6ad40d71efc4956c6ed83ac7b5a8b2822a
Caputo <rcaputo2@gs66-mew.fios-router.home>
2018-07-01 11:56:09 -0400

    adding in effective area files

---
+++ aeff/plotaeff.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+
+def plotAeffvsZenith(zenith, aeff, doshow=True, dosave=False):
+
+	plt.figure(figsize=(8,6))
+
+	#plt.title(r'Effective Area vs. Zenith')
+
+	plt.scatter(zenith, aeff, color='blue')
+	plt.plot(zenith, aeff, color='blue', alpha=0.5, linestyle='--', lw=2, label="PixEye")
+
+	plt.xlabel('Zenith angle ($^{\circ}$)', fontsize=16)
+	plt.ylabel('Effective Area (cm$^2$)', fontsize=16)
+
+	plt.xlim(0.0,65.0)
+	plt.ylim(2.0,5.0)
+
+	plt.legend(loc='lower center', prop={'size': 16}, numpoints=1,
+               frameon=False)
+
+	if dosave:
+		plt.savefig("PixEye_Aeff_vs_zenith.png")
+
+	if doshow: 
+		plt.show()
+
+	
+
+
