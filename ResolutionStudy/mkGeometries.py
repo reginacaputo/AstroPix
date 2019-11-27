823d8762864e0416e7ac0af1b446b5561b163a21
nmik <michela.negro@to.infn.it>
2019-11-27 13:34:43 -0500

    now creates the folders for the geo configs

--- ResolutionStudy/mkGeometries.py
+++ ResolutionStudy/mkGeometries.py
@@ -6,26 +6,54 @@
 #-------------------------------------------------------------------#
 #*******************************************************************#
 
+import os
+import argparse
+import numpy as np
+from importlib.machinery import SourceFileLoader
 
-from config import *
+__description__ = 'Creates the AstroPix geometry configurations'
 
-def run_mkGeometries():
-	for i, p in enumerate(PASSIVE):
+formatter = argparse.ArgumentDefaultsHelpFormatter
+PARSER = argparse.ArgumentParser(description=__description__, formatter_class=formatter)
+PARSER.add_argument('-c', '--config', type=str, required=True,
+                    help='the input configuration file')
+
+def get_var_from_file(filename):
+    f = open(filename)
+    global data
+    data = SourceFileLoader('data', filename).load_module()
+    f.close()
+    
+def run_mkGeometries(**kwargs):
+	assert(kwargs['config'].endswith('.py'))
+	get_var_from_file(kwargs['config'])
+	passive = data.PASSIVE
+	thickness = data.THICKNESS
+	voxelsize = data.VOXELSIZE
+	for i, p in enumerate(passive):
 		print('PASSIVE %i: %i%%' %(i, p))
-		for j, t in enumerate(THICKNESS):	
+		for j, t in enumerate(thickness):	
 			print('THICKNESS %i: %i um' %(j, t))
-			for k, v in enumerate(VOXELSIZE): 
+			for k, v in enumerate(voxelsize): 
 				print('VOXEL SIZE %i: %.2f mm' %(k, v))
+				
+				folder_name = 'geo_%i_%i_%.2f' %(p, t, v)
+				if not os.path.exists('geometry/%s' %folder_name):
+					os.system('mkdir geometry/%s' %folder_name)
+				                  
 		print('\n')
 				
 				
 				
 if __name__ == '__main__':
+	args = PARSER.parse_args()
+	
 	print("--------------------")
 	print("---- Start  Run ----")
 	print("--------------------")
+
+	run_mkGeometries(**args.__dict__)
 	
-	run_mkGeometries()
 	print("--------------------")
 	print("----- End  Run -----")
 	print("--------------------")
